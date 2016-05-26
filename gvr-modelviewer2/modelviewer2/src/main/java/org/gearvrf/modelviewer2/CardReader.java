f529b024d3ea7875e79fed6bf359791d450a5f09
apjagdale <a.jagdale@partner.samsung.com>
2016-05-25 18:12:04 -0700

    Adding new demo of ModelViewer.

---
+++ gvr-modelviewer2/modelviewer2/src/main/java/org/gearvrf/modelviewer2/CardReader.java
@@ -0,0 +1,40 @@
+package org.gearvrf.modelviewer2;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+/**
+ /* Copyright 2015 Samsung Electronics Co., LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+public class CardReader {
+
+    private String myDirectory;
+    private String myExtension;
+
+    public CardReader(String sDirectory, String sExtension){
+        myDirectory = sDirectory;
+        myExtension = sExtension;
+    }
+    public File[] getModels(){
+        File directory = new File(myDirectory);
+        FilenameFilter filter = new FilterFiles(myExtension);
+        File list[] = directory.listFiles();
+
+        return list;
+    }
+
+}
