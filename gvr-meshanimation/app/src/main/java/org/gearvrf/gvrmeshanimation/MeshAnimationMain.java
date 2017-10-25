3e65e0f840585bc529bb61881cc9d46ae6695415
Abhijit <a.jagdale@samsung.com>
2017-10-25 11:08:16 -0700

    Workaround for the SceneObject resetting to 0,0,0 on starting the animation

--- gvr-meshanimation/app/src/main/java/org/gearvrf/gvrmeshanimation/MeshAnimationMain.java
+++ gvr-meshanimation/app/src/main/java/org/gearvrf/gvrmeshanimation/MeshAnimationMain.java
@@ -3,21 +3,29 @@ package org.gearvrf.gvrmeshanimation;
 import java.io.IOException;
 
 import org.gearvrf.GVRActivity;
+import org.gearvrf.GVRColorShader;
 import org.gearvrf.GVRContext;
+import org.gearvrf.GVRMaterial;
+import org.gearvrf.GVRMesh;
+import org.gearvrf.GVRMeshCollider;
+import org.gearvrf.GVRRenderData;
 import org.gearvrf.GVRScene;
 import org.gearvrf.GVRMain;
+import org.gearvrf.GVRSceneObject;
+import org.gearvrf.GVRShaderId;
 import org.gearvrf.animation.GVRAnimation;
 import org.gearvrf.animation.GVRAnimationEngine;
 import org.gearvrf.animation.GVRAnimator;
 import org.gearvrf.animation.GVRRepeatMode;
 import org.gearvrf.scene_objects.GVRModelSceneObject;
+import org.joml.Matrix4f;
 
+import android.graphics.Color;
 import android.util.Log;
 
 public class MeshAnimationMain extends GVRMain {
 
     private GVRContext mGVRContext;
-    private GVRModelSceneObject mCharacter;
 
     private final String mModelPath = "TRex_NoGround.fbx";
 
@@ -27,7 +35,8 @@ public class MeshAnimationMain extends GVRMain {
 
     private GVRAnimationEngine mAnimationEngine;
     GVRAnimator mAssimpAnimation = null;
-    
+
+    GVRSceneObject menuButton;
 
     public MeshAnimationMain(GVRActivity activity) {
         mActivity = activity;
@@ -38,26 +47,45 @@ public class MeshAnimationMain extends GVRMain {
         mGVRContext = gvrContext;
         mAnimationEngine = gvrContext.getAnimationEngine();
 
-        GVRScene mainScene = gvrContext.getMainScene();
-
-
+        gvrContext.getMainScene().getMainCameraRig().getLeftCamera().setBackgroundColor(Color.WHITE);
+        gvrContext.getMainScene().getMainCameraRig().getRightCamera().setBackgroundColor(Color.WHITE);
         try {
-            mCharacter = gvrContext.getAssetLoader().loadModel(mModelPath, mainScene);
-            mCharacter.getTransform().setPosition(0.0f, -10.0f, -10.0f);
-            mCharacter.getTransform().setRotationByAxis(90.0f, 1.0f, 0.0f, 0.0f);
-            mCharacter.getTransform().setRotationByAxis(40.0f, 0.0f, 1.0f, 0.0f);
-            mCharacter.getTransform().setScale(1.5f, 1.5f, 1.5f);
 
-            mAssimpAnimation = (GVRAnimator) mCharacter.getComponent(GVRAnimator.getComponentType());
+            GVRMesh sMesh = gvrContext.createQuad(4,2);
+            /*
+                menuButton is the root which has a mesh of a quad to it.
+                It also has a child at index 0 which is the FBX model(TRex)
+                Whenever you want to make changes to the Trex model do menuButton.getChildByIndex(0) or have its reference
+                    > childTrexModel = menuButton.getChildByIndex(0);
+            */
+            menuButton = gvrContext.getAssetLoader().loadModel("TRex_NoGround.fbx", gvrContext.getMainScene());
+            mAssimpAnimation = (GVRAnimator) menuButton.getComponent(GVRAnimator.getComponentType());
             mAssimpAnimation.setRepeatMode(GVRRepeatMode.REPEATED);
             mAssimpAnimation.setRepeatCount(-1);
+
+            // Getting the TrexModel
+            GVRSceneObject childTrexModel = menuButton.getChildByIndex(0);
+            childTrexModel.getTransform().rotateByAxis(90,0,1,0);
+
+            // This menuButton is a Quad and also the root SceneObject
+            menuButton.getTransform().setPosition(0, 0, -10);
+            menuButton.attachRenderData(new GVRRenderData(gvrContext));
+            menuButton.getRenderData().setMaterial(new GVRMaterial(gvrContext, new GVRShaderId(GVRColorShader.class)));
+            menuButton.getRenderData().getMaterial().setColor(1,0,0f);
+            menuButton.getRenderData().setMesh(sMesh);
+
+            menuButton.getRenderData().setRenderingOrder(10000);
+
+            // Start the animation
+            mAssimpAnimation.start();
+
         } catch (IOException e) {
             e.printStackTrace();
             mActivity.finish();
             mActivity = null;
             Log.e(TAG, "One or more assets could not be loaded.");
         }
-        mAssimpAnimation.start();
+
     }
 
     @Override
